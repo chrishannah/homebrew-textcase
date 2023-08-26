5ff31cc152bb1748991383a3602a3d2fb42aff65
Christopher Hannah <me@chrishannah.me>
2023-08-26 21:48:02 +0100

    update sha

--- Formula/textcase.rb
+++ Formula/textcase.rb
@@ -2,7 +2,7 @@ class Textcase < Formula
   desc "Text transformation tool"
   homepage "https://github.com/chrishannah/textcase-cli"
   url "https://github.com/chrishannah/textcase-cli/archive/refs/tags/1.0.tar.gz"
-  sha256 "d0c4e51db7fef8cea375ac4ab08790056a0cff240defc311baa48fd8a66fff82"
+  sha256 "e7daff41716d092b07b60da93b0731ba276207685521c2dee0efd03b4616aae1"
   license "GPL-3.0-only"
 
   depends_on xcode: ["14.0", :build]
