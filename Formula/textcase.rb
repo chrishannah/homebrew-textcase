be00e111eb764716d2815a2ad42e51f9eee420fa
Christopher Hannah <me@chrishannah.me>
2023-08-29 23:13:37 +0100

    1.1

--- Formula/textcase.rb
+++ Formula/textcase.rb
@@ -1,8 +1,8 @@
 class Textcase < Formula
   desc "Text transformation tool"
   homepage "https://github.com/chrishannah/textcase-cli"
-  url "https://github.com/chrishannah/textcase-cli/archive/refs/tags/1.0.tar.gz"
-  sha256 "ecad14bd576eee6c3652e0a412d056571c7dfd1a66cc86a042199a9d8b5a6693"
+  url "https://github.com/chrishannah/textcase-cli/archive/refs/tags/1.1.tar.gz"
+  sha256 "06d68926e97c6462e93ee0a717a3cb203132b0cfd5e6a6d606ab61e06945bfc4"
   license "GPL-3.0-only"
 
   depends_on :macos
