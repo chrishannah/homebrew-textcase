8602f113fe4fdd2332db3b7c25dfe3ae4289c1e1
Christopher Hannah <me@chrishannah.me>
2023-09-03 22:01:28 +0100

    update to version 1.3

--- Formula/textcase.rb
+++ Formula/textcase.rb
@@ -1,8 +1,8 @@
 class Textcase < Formula
   desc "Text transformation tool"
   homepage "https://github.com/chrishannah/textcase-cli"
-  url "https://github.com/chrishannah/textcase-cli/archive/refs/tags/1.2.tar.gz"
-  sha256 "9543378c17ab133a3e1635c8d2c478c8b7350b1e014d181f995c5ecf7468e918"
+  url "https://github.com/chrishannah/textcase-cli/archive/refs/tags/1.3.tar.gz"
+  sha256 "964a9dc5a9a8c852dc0fcd7886d81759f88509c1fb7552249b2b04ed34d4e7de"
   license "GPL-3.0-only"
 
   depends_on :macos
@@ -16,8 +16,8 @@ class Textcase < Formula
 
   test do
     # print the help text
-    system "#{bin}/textcase", "--help"
+    system "#{bin}/textcase", "format --help"
     # print the list of available formats
-    system "#{bin}/textcase", "--listFormats"
+    system "#{bin}/textcase", "list-formats"
   end
 end
